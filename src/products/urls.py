09ca9a5adbbfe75ea0fa91689b20733be9a68b64
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2024-12-23 22:03:54 +0000

    Editted the CategoryAPIViews and routes and created the manufacturer apiviews and routes

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import CategoryAPIView, get_categories
+from .views import CategoryAPIView, CategoryListAPIView, ManufacturerListAPIView, ManufacturerAPIView
 
 urlpatterns = [
-    path('categories', get_categories, name='get_categories'),
+    path('categories', CategoryListAPIView.as_view(), name='get_categories'),
     path('category/<int:id>', CategoryAPIView.as_view(), name='categories'),
-    path('category', CategoryAPIView.as_view(), name='add_category'),
+    path('category', CategoryAPIView.as_view(), name='add_category'), #For post request
+    path('manufacturers', ManufacturerListAPIView.as_view(), name='get_manufacturers'),
+    path('manufacturer', ManufacturerAPIView.as_view(), name='add_manufacturer'),
+    path('manufacturer/<int:id>', ManufacturerAPIView.as_view(), name='add_manufacturer'), #For post request
+
 ]
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from .serializers import CategorySerializer, ProductSerializer, ManufacturerSerializer
@@ -7,11 +6,12 @@ from .models import Category, Product, Manufacturer
 from django.http import Http404
 # Create your views here.
 
-@api_view(['GET'])
-def get_categories(request):
-    categories = Category.objects.all()
-    categories_serializer = CategorySerializer(categories, many=True)
-    return Response(categories_serializer.data, status=status.HTTP_200_OK)
+class CategoryListAPIView(APIView):
+
+    def get(self, request, format=None):
+        category = Category.objects.all()
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class CategoryAPIView(APIView):
@@ -48,4 +48,49 @@ class CategoryAPIView(APIView):
         except Category.DoesNotExist as error:
             return Response({"error": str(error)}, status=status.HTTP_404_NOT_FOUND)
         category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ManufacturerListAPIView(APIView):
+
+    def get(self, request, format=None):
+        manufacturers = Manufacturer.objects.all()
+        serializer = ManufacturerSerializer(manufacturers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class ManufacturerAPIView(APIView):
+
+    def get(self, request, id, format=None):
+        try:
+            manufacturer = Manufacturer.objects.get(id=id)
+        except Manufacturer.DoesNotExist as error:
+            return Response({"error":str(error)}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ManufacturerSerializer(manufacturer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+        serializer = ManufacturerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, id, format=None):
+        try:
+            manfacturer = Manufacturer.objects.get(id=id)
+        except Manufacturer.DoesNotExist as error:
+            return Response({"error":str(error)}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ManufacturerSerializer(manfacturer, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id, format=None):
+        try:
+            manufacturer = Manufacturer.objects.get(id=id)
+        except Manufacturer.DoesNotExist as error:
+            return Response({"error":str(error)}, status=status.HTTP_404_NOT_FOUND)
+        manufacturer.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
