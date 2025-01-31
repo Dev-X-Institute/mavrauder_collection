afc84f11b6a5b9ef0171493930f106818983c8f1
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-01-31 04:03:03 +0000

    feat: Update cart URLs to use DefaultRouter and modify endpoint paths

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import CartAPIView, CartItemViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'cart/item', CartItemViewSet, basename='cartitem')
 
 urlpatterns = [
-    path('cart', CartAPIView.as_view(), name='get_cart'),
-    path('cart-item', CartItemViewSet.as_view({
-        'get':'list',
-        'post':'create',
-    }), name='add_cart_item'),
-    path('cart-item/<int:id>', CartItemViewSet.as_view({'delete':'destroy'}), name='remove_cart_item'),
-]
+    path('details', CartAPIView.as_view(), name='get_cart'),
+    path('', include(router.urls))
+] 
