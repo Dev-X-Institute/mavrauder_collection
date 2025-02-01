0a63c3fa5c715ab0751af62134147d0d87dc4d55
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-02-01 13:48:07 +0000

    feat: Adding SessionAuthentication to project

--- src/config/settings.py
+++ src/config/settings.py
@@ -70,8 +70,9 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework_simplejwt.authentication.JWTAuthentication',
+       'rest_framework.authentication.SessionAuthentication'
     )
-}
+}   
 
 DJOSER = {
 
